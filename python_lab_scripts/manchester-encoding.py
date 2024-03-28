9026ae3669b60abcc73f57695e960d92c3651706
zzems18 <153057462+zzems18@users.noreply.github.com>
2024-03-28 14:46:39 -0500

    Update manchester-encoding.py

--- python_lab_scripts/manchester-encoding.py
+++ python_lab_scripts/manchester-encoding.py
@@ -1,12 +1,19 @@
 import manchester_code
 from manchester_code import encode, decode
 
+
+#used to encode data already listed in bytes
 manchester_code = encode([0b00001111, 0b01101001])
 print(''.join(['{:08b}'.format(m) for m in manchester_code]))
 # 01010101101010100110100110010110
 
-message= input("what is the message that is being encoded:")
+#decode message and list in bytes
+dmsg= decode([0b01010101, 0b10101010, 0b01101001, 0b10010110])
+print(''.join(['{:08b}'.format(m) for m in dmsg]))
 
+
+#used to take a user string message and encode and decode it
+message= input("what is the message that is being encoded:")
 
 msg =(message.encode(encoding = 'UTF-8'))
 encoded = encode(msg)
